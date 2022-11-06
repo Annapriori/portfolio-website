14f2fd71ca57e7f9ff24b249c632767eb74fd744
Annapriori <114689078+Annapriori@users.noreply.github.com>
2022-11-06 22:29:12 +0300

    Added hamburger menu
    Linked Jquery to all HTML pages
    Added additional li to the nav menu for the drop-down menu
    added code to JS for the drop-down menu

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,12 @@
 console.log ("Welcome to Anna Eivör's Portfolio")
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    //I want my logo also dessapear when menu opens. I wanted to add here smth like logo=none, but it didnt work.
+  } else {
+    x.className = 'navtoggle';
+  }
+}
