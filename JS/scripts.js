d5ead291342afc2ae4071dc90675b764069eb5e7
Annapriori <114689078+Annapriori@users.noreply.github.com>
2022-11-14 13:55:42 +0300

    Changes as per the call with my mentor

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,3 +10,20 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+    var icon=document.getElementsByClassName("fa-bars")[0];
+    if(icon){
+       icon.className = icon.className.replace("fa-bars", "fa-xmark");
+    }
+    else{
+    icon=document.getElementsByClassName("fa-xmark")[0];
+     icon.className = icon.className.replace("fa-xmark", "fa-bars");
+    }
+}
