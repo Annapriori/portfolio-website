b4f2fb2786611e565ac282c7ced61f1e2c025f9b
Annapriori <114689078+Annapriori@users.noreply.github.com>
2022-11-28 17:38:24 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-console.log("Welcome to Anna Eivör's Portfolio")
+console.log ("Welcome to Anna Eivör's Portfolio")
 
 
-function menuToggle(...args: []) {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
+    //I want my logo also dessapear when menu opens. I wanted to add here smth like logo=none, but it didnt work.
   } else {
     x.className = 'navtoggle';
   }
-  var icon = document.getElementsByClassName("fa-bars")[0];
-  if (icon) {
-    icon.className = icon.className.replace("fa-bars", "fa-xmark");
-  }
-  else {
-    icon = document.getElementsByClassName("fa-xmark")[0];
-    icon.className = icon.className.replace("fa-xmark", "fa-bars");
-  }
+}
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+    var icon=document.getElementsByClassName("fa-bars")[0];
+    if(icon){
+       icon.className = icon.className.replace("fa-bars", "fa-xmark");
+    }
+    else{
+    icon=document.getElementsByClassName("fa-xmark")[0];
+     icon.className = icon.className.replace("fa-xmark", "fa-bars");
+    }
 }
 
 window.onload = () => {
@@ -23,21 +33,19 @@ window.onload = () => {
   let all = document.getElementsByClassName("zoomE");
 
   // (B) CLICK TO GO FULLSCREEN
-  if (all.length > 0) {
-    for (let i of all) {
-      i.onclick = () => {
-        // (B1) EXIT FULLSCREEN
-        if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
-          if (document.exitFullscreen) { document.exitFullscreen(); }
-          else { document.webkitCancelFullScreen(); }
-        }
+  if (all.length>0) { for (let i of all) {
+    i.onclick = () => {
+      // (B1) EXIT FULLSCREEN
+      if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
+        if (document.exitFullscreen) { document.exitFullscreen(); }
+        else { document.webkitCancelFullScreen(); }
+      }
 
-        // (B2) ENTER FULLSCREEN
-        else {
-          if (i.requestFullscreen) { i.requestFullscreen(); }
-          else { i.webkitRequestFullScreen(); }
-        }
-      };
-    }
-  }
+      // (B2) ENTER FULLSCREEN
+      else {
+        if (i.requestFullscreen) { i.requestFullscreen(); }
+        else { i.webkitRequestFullScreen(); }
+      }
+    };
+  }}
 };
