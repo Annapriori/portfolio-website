a6d6080e1a72f9a6e2fed6ffd886a55176f6423c
Annapriori <114689078+Annapriori@users.noreply.github.com>
2022-12-04 00:31:18 +0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log ("Welcome to Anna Eivör's Portfolio")
+console.log("Welcome to Anna Eivör's Portfolio")
 
 
 function menuToggle() {
@@ -12,20 +12,20 @@ function menuToggle() {
 }
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
-    var icon=document.getElementsByClassName("fa-bars")[0];
-    if(icon){
-       icon.className = icon.className.replace("fa-bars", "fa-xmark");
-    }
-    else{
-    icon=document.getElementsByClassName("fa-xmark")[0];
-     icon.className = icon.className.replace("fa-xmark", "fa-bars");
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+  var icon = document.getElementsByClassName("fa-bars")[0];
+  if (icon) {
+    icon.className = icon.className.replace("fa-bars", "fa-xmark");
+  }
+  else {
+    icon = document.getElementsByClassName("fa-xmark")[0];
+    icon.className = icon.className.replace("fa-xmark", "fa-bars");
+  }
 }
 
 window.onload = () => {
@@ -33,19 +33,21 @@ window.onload = () => {
   let all = document.getElementsByClassName("zoomE");
 
   // (B) CLICK TO GO FULLSCREEN
-  if (all.length>0) { for (let i of all) {
-    i.onclick = () => {
-      // (B1) EXIT FULLSCREEN
-      if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
-        if (document.exitFullscreen) { document.exitFullscreen(); }
-        else { document.webkitCancelFullScreen(); }
-      }
+  if (all.length > 0) {
+    for (let i of all) {
+      i.onclick = () => {
+        // (B1) EXIT FULLSCREEN
+        if (document.fullscreenElement != null || document.webkitFullscreenElement != null) {
+          if (document.exitFullscreen) { document.exitFullscreen(); }
+          else { document.webkitCancelFullScreen(); }
+        }
 
-      // (B2) ENTER FULLSCREEN
-      else {
-        if (i.requestFullscreen) { i.requestFullscreen(); }
-        else { i.webkitRequestFullScreen(); }
-      }
-    };
-  }}
+        // (B2) ENTER FULLSCREEN
+        else {
+          if (i.requestFullscreen) { i.requestFullscreen(); }
+          else { i.webkitRequestFullScreen(); }
+        }
+      };
+    }
+  }
 };
